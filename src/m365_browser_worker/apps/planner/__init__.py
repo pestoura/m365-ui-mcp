5a811c0d0ab3b966573866cfcd46a111783f6fc3
Pedro <pedro.estoura@gmail.com>
2026-08-09 00:13:08 +0100

    refactor(planner): export worker adapter

---
+++ src/m365_browser_worker/apps/planner/__init__.py
@@ -0,0 +1,5 @@
+"""Planner browser-worker adapter boundary."""
+
+from m365_browser_worker.apps.planner.adapter import PlannerDataProvider, PlannerWorkerAdapter
+
+__all__ = ["PlannerDataProvider", "PlannerWorkerAdapter"]
